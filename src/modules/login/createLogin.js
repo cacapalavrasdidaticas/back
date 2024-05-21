295e1f7e309a4e2e8068b07a7b774174363424c3
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-05-21 00:27:48 -0300

    Update createLogin.js

--- src/modules/login/createLogin.js
+++ src/modules/login/createLogin.js
@@ -19,14 +19,16 @@ export async function criarConta(dados) {
     // Criptografar a senha antes de armazená-la no banco de dados
     const hashedPassword = await bcrypt.hash(senha, 10);
 
-    // Inserir os dados do novo usuário na tabela de login
-    await db.none("INSERT INTO login (login, senha, cpf) VALUES ($1, $2, $3)", [userLogin, hashedPassword, cpf]);
-
     // Inserir os dados na tabela de contas
     await db.none(
         `INSERT INTO contas (nome, sexo, data_nascimento, email, cpf, telefone_celular, endereco, bairro, cidade_uf, cep, pais)
          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`,
         [nome, sexo, dataNascimento, email, cpf, telefoneCelular, endereco, bairro, cidadeUF, cep, pais]
     );
+
+    // Inserir os dados do novo usuário na tabela de login
+    await db.none("INSERT INTO login (login, senha, cpf) VALUES ($1, $2, $3)", [userLogin, hashedPassword, cpf]);
+
+    
 }
 
