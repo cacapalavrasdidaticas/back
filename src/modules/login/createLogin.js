d0967d21978016f0f1a034b0d4ef723205ce0ed8
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-05-21 00:41:47 -0300

    Update createLogin.js

--- src/modules/login/createLogin.js
+++ src/modules/login/createLogin.js
@@ -1,34 +1,19 @@
 import db from "../../db.js";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 
-const JWT_SECRET = 'seu_segredo_secreto'; // Chave secreta para assinar o token JWT
+export async function criarConta(usuario) {
+    const { nome, sexo, dataNascimento, email, cpf, telefoneCelular, endereco, senha } = usuario;
+    const hashedSenha = await bcrypt.hash(senha, 10);
 
-export async function criarConta(dados) {
-    const { email, senha, nome, sexo, dataNascimento, cpf, telefoneCelular, login, endereco, bairro, cidadeUF, cep, pais } = dados;
-    console.log("Recebido em criarConta:", dados);
+    try {
+        const novaConta = await db.one(
+            `INSERT INTO contas (nome, sexo, dataNascimento, email, cpf, telefoneCelular, endereco, senha)
+             VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id`,
+            [nome, sexo, dataNascimento, email, cpf, telefoneCelular, endereco, hashedSenha]
+        );
 
-    const userLogin = login || email;
-
-    // Verificar se o login já existe
-    const existingUser = await db.oneOrNone("SELECT * FROM login WHERE login = $1", [userLogin]);
-    if (existingUser) {
-        throw new Error("O login já está em uso.");
+        return { id: novaConta.id };
+    } catch (error) {
+        throw error;
     }
-
-    // Criptografar a senha antes de armazená-la no banco de dados
-    const hashedPassword = await bcrypt.hash(senha, 10);
-
-    // Inserir os dados na tabela de contas
-    await db.none(
-        `INSERT INTO contas (nome, sexo, data_nascimento, email, cpf, telefone_celular, endereco, bairro, cidade_uf, cep, pais)
-         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`,
-        [nome, sexo, dataNascimento, email, cpf, telefoneCelular, endereco, bairro, cidadeUF, cep, pais]
-    );
-
-    // Inserir os dados do novo usuário na tabela de login
-    await db.none("INSERT INTO login (login, senha, cpf) VALUES ($1, $2, $3)", [userLogin, hashedPassword, cpf]);
-
-    
 }
-
