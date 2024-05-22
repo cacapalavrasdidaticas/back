402358d94daa3dd90db67c28f9533cd0c3f99565
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-05-21 22:11:38 -0300

    Update getAllAssociations.js

--- src/modules/pdf/getAllAssociations.js
+++ src/modules/pdf/getAllAssociations.js
@@ -3,11 +3,14 @@ import db from "../../db.js";
 export async function obterAssociacoes() {
   try {
     const associacoes = await db.any(`
-      SELECT pdfs.id, pdfs.nome_do_arquivo, pdf_descriptions.descricao, pdf_descriptions.fotos
-      FROM pdfs
-      LEFT JOIN pdf_descriptions ON pdfs.id = pdf_descriptions.pdf_id
+      SELECT pd.id, p.nome_do_arquivo, pd.descricao, pd.fotos
+      FROM pdf_descriptions pd
+      JOIN pdfs p ON pd.pdf_id = p.id
     `);
-    return associacoes;
+    return associacoes.map(assoc => ({
+      ...assoc,
+      fotos: assoc.fotos ? assoc.fotos.slice(1, -1).split(',').map(foto => foto.trim().replace(/"/g, '')) : []
+    }));
   } catch (error) {
     console.log("Erro ao obter associações:", error);
     throw error;
