8ee4aadd311ee8110211a9ce5a3ab36afd29fbac
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-05-21 21:54:11 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Usar diretório temporário em ambientes serverless
-const uploadDir = path.join('/tmp', 'uploads');
+const uploadDir = path.join(__dirname, 'uploads'); // Atualizar para usar diretório relativo ao projeto
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }
@@ -41,7 +41,7 @@ app.use(validateApiKey);
 // Configuração do multer para armazenamento em disco
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, uploadDir); // Usar o diretório temporário
+    cb(null, uploadDir); // Usar o diretório relativo ao projeto
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
