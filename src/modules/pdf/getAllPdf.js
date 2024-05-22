5f77851a0797603a96232760a2e8a3fedb2916ef
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-05-21 21:15:57 -0300

    Update getAllPdf.js

--- src/modules/pdf/getAllPdf.js
+++ src/modules/pdf/getAllPdf.js
@@ -2,7 +2,7 @@ import db from "../../db.js";
 
 export async function obterTodosPDFs() {
   try {
-    const pdfs = await db.any("SELECT * FROM pdfs");
+    const pdfs = await db.any("SELECT id, nome_do_arquivo FROM pdfs");
     return pdfs;
   } catch (error) {
     console.log("Erro ao obter todos os PDFs:", error);
