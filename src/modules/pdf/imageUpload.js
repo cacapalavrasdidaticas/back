a7d25f750daff6112783e04a79f97f2b3247147c
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-05-22 00:23:22 -0300

    Update imageUpload.js

--- src/modules/pdf/imageUpload.js
+++ src/modules/pdf/imageUpload.js
@@ -7,13 +7,17 @@ const upload = multer({ storage: storage });
 
 // Função para fazer upload da imagem
 const uploadImage = async (req, res) => {
+  const { title, description, price, link, imageAlt } = req.body;
   const file = req.file;
   if (!file) {
     return res.status(400).json({ error: 'Nenhum arquivo foi enviado' });
   }
   
   try {
-    const result = await db.query('INSERT INTO images (image) VALUES ($1) RETURNING id', [file.buffer]);
+    const result = await db.query(
+      'INSERT INTO images (image, title, description, price, link, imageAlt) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id',
+      [file.buffer, title, description, price, link, imageAlt]
+    );
     if (result && result.length > 0) {
       res.json({ id: result[0].id });
     } else {
@@ -29,11 +33,12 @@ const uploadImage = async (req, res) => {
 const getImage = async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await db.query('SELECT image FROM images WHERE id = $1', [id]);
+    const result = await db.query('SELECT image, title, description, price, link, imageAlt FROM images WHERE id = $1', [id]);
     if (result && result.length > 0) {
-      const image = result[0].image;
+      const { image, title, description, price, link, imageAlt } = result[0];
       res.set('Content-Type', 'image/jpeg');
       res.send(image);
+      // Você pode adicionar outras informações no cabeçalho ou corpo da resposta, conforme necessário
     } else {
       res.status(404).json({ error: 'Imagem não encontrada' });
     }
@@ -46,7 +51,7 @@ const getImage = async (req, res) => {
 // Função para listar todas as imagens
 const getAllImages = async (req, res) => {
   try {
-    const result = await db.query('SELECT id FROM images');
+    const result = await db.query('SELECT id, title, description, price, link, imageAlt FROM images');
     if (result) {
       res.json(result);
     } else {
