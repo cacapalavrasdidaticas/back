45ecb38eebdc89d00e89f544d9b169d0ab0c12da
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-05-21 21:27:44 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import multer from 'multer';
+import path from 'path';
 import validateApiKey from './middleware.js';
 
 import { obterPDF } from './modules/pdf/getPdf.js';
