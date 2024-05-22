8179c27df3e7bd59b523a0b7800cdbc308dd34b9
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-05-21 22:20:51 -0300

    Update getAllAssociations.js

--- src/modules/pdf/getAllAssociations.js
+++ src/modules/pdf/getAllAssociations.js
@@ -1,14 +1,12 @@
 import db from "../../db.js";
 
-export async function obterAssociacoes(page = 1, limit = 10) {
+export async function obterAssociacoes() {
   try {
-    const offset = (page - 1) * limit;
     const associacoes = await db.any(`
       SELECT pd.id, p.nome_do_arquivo, pd.descricao, pd.fotos
       FROM pdf_descriptions pd
       JOIN pdfs p ON pd.pdf_id = p.id
-      LIMIT $1 OFFSET $2
-    `, [limit, offset]);
+    `);
 
     // Mapear as associações e garantir que fotos seja um array
     return associacoes.map(assoc => ({
