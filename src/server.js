3efd557e95e4fcb0d791f6732ac5b027e9393761
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-08-19 23:23:26 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ import { associatePdf } from './modules/pdf/postPdfDescription.js';
 import { associatePdfWithImage } from './modules/pdf/getAllAssociations.js';
 import { upload, uploadImage, getImage, getAllImages } from './modules/pdf/imageUpload.js';
 import { getPdfImageAssociations } from './modules/pdf/getPdfImageAssociations.js';
-
+import { atualizarConta } from './modules/login/putUser.js';
 const app = express();
 
 // Obter o diretório atual
