c95737d0cb840a6190f6d71911e6220c7e494712
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-08-22 23:41:06 -0300

    Update getAccount.js

--- src/modules/login/getAccount.js
+++ src/modules/login/getAccount.js
@@ -1,44 +1,18 @@
 import db from "../../db.js";
-import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"; 
 
-app.get('/conta/:id', async (req, res) => {
-    const { id } = req.params;
-
+export async function loginUsuario(id) {
     try {
-        // Consulta SQL para obter os dados da conta pelo ID
-        const query = `
-            SELECT 
-                id, 
-                nome, 
-                senha, 
-                datanascimento, 
-                email, 
-                cpf, 
-                telefonecelular, 
-                sexo, 
-                rua, 
-                bairro, 
-                cidadeuf, 
-                cep, 
-                pais
-            FROM contas
-            WHERE id = $1;
-        `;
+        const usuario = await db.oneOrNone("SELECT * FROM contas WHERE id = $1", [id]);
 
-        // Executa a consulta com o ID fornecido
-        const conta = await db.oneOrNone(query, [id]);
-
-        if (!conta) {
-            // Retorna 404 se a conta não for encontrada
-            return res.status(404).json({ error: 'Conta não encontrada' });
+        if (!usuario) {
+            throw new Error("Usuário não encontrado");
         }
 
-        // Retorna os dados da conta
-        res.status(200).json(conta);
+        return usuario
+        
     } catch (error) {
-        console.error("Erro ao buscar conta:", error.message);
-
-        // Retorna 500 em caso de erro interno do servidor
-        res.status(500).json({ error: 'Erro ao buscar conta', message: error.message });
+        console.error("Erro ao buscar o usuário e gerar o token:", error.message);
+        throw error;
     }
-});
+}
