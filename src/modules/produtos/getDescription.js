a5b3ed11c6a95c041549d506b7f9e0d6fe2ec2c6
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-08-26 23:50:15 -0300

    Add files via upload

---
+++ src/modules/produtos/getDescription.js
@@ -0,0 +1,17 @@
+import db from "../../db.js";
+
+export async function obterTodosProdutosDescricao() {
+    try {
+        const produtos = await db.any(`
+            SELECT p.id, p.nome_produto, p.descricao, p.categoria, p.nivel_ensino, p.valor, p.componente_curricular, p.pdf,
+                   json_agg(encode(f.foto, 'base64')) AS fotos
+            FROM produtos p
+            LEFT JOIN fotos_produtos f ON p.id = f.produto_id
+            GROUP BY p.id
+        `);
+        return produtos;
+    } catch (error) {
+        console.error("Erro ao buscar produtos:", error);
+        throw error;
+    }
+}
