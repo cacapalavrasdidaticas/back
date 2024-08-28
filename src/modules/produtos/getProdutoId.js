d5b9c03a6c5471858eed23e3bf715fb032283305
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-08-27 23:23:33 -0300

    Update getProdutoId.js

--- src/modules/produtos/getProdutoId.js
+++ src/modules/produtos/getProdutoId.js
@@ -1,15 +1,17 @@
-// modules/produto/getProduto.js
 import db from "../../db.js";
 
 export async function obterProduto(id) {
     try {
         const produto = await db.oneOrNone(`
-            SELECT id, nome_produto, descricao, categoria, nivel_ensino, valor, componente_curricular, fotos
-            FROM produtos
-            WHERE id = $1
+            SELECT p.id, p.nome_produto, p.descricao, p.categoria, p.nivel_ensino, p.valor, p.componente_curricular,
+                   json_agg(encode(f.foto, 'base64')) AS fotos
+            FROM produtos p
+            LEFT JOIN fotos_produtos f ON p.id = f.produto_id
+            WHERE p.id = $1
+            GROUP BY p.id
         `, [id]);
 
-        console.log(produto)
+        console.log(produto);
 
         return produto;
     } catch (error) {
