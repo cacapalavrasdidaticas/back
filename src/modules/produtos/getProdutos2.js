436c409d3a73a4077db3e3bce2bd80007c269f05
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-09-05 01:21:12 -0300

    Add files via upload

---
+++ src/modules/produtos/getProdutos2.js
@@ -0,0 +1,19 @@
+import db from "../../db.js";
+
+// Função para obter todos os produtos com fotos e PDFs
+export async function obterTodosProdutos() {
+    try {
+        const produtos = await db.any(`
+            SELECT p.id, p.nome_produto, p.descricao, p.categoria, p.nivel_ensino, p.valor, p.componente_curricular, 
+                   encode(p.pdf, 'base64') AS pdf,  -- Codificar o PDF para Base64
+                   COALESCE(json_agg(encode(f.foto, 'base64')) FILTER (WHERE f.foto IS NOT NULL), '[]') AS fotos  -- Codificar as fotos para Base64
+            FROM produtos p
+            LEFT JOIN fotos_produtos f ON p.id = f.produto_id
+            GROUP BY p.id
+        `);
+        return produtos;
+    } catch (error) {
+        console.error("Erro ao buscar produtos:", error);
+        throw error;
+    }
+}
