55b0ef280031704b84f35cff9f4589f9205d0b2e
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-09-05 01:19:56 -0300

    Add files via upload

--- src/modules/produtos/getProdutos.js
+++ src/modules/produtos/getProdutos.js
@@ -1,26 +1,12 @@
-// modules/produto/getProdutos.js
 import db from "../../db.js";
 
-// export async function obterTodosProdutos() {
-//     try {
-//         const produtos = await db.any(`
-//             SELECT p.id, p.nome_produto, p.descricao, p.categoria, p.nivel_ensino, p.valor, p.componente_curricular, p.pdf,
-//                    json_agg(f.foto) AS fotos
-//             FROM produtos p
-//             LEFT JOIN fotos_produtos f ON p.id = f.produto_id
-//             GROUP BY p.id
-//         `);
-//         return produtos;
-//     } catch (error) {
-//         console.error("Erro ao buscar produtos:", error);
-//         throw error;
-//     }
-// }
+// Função para obter todos os produtos com fotos e PDFs
 export async function obterTodosProdutos() {
     try {
         const produtos = await db.any(`
-            SELECT p.id, p.nome_produto, p.descricao, p.categoria, p.nivel_ensino, p.valor, p.componente_curricular, p.pdf,
-                   json_agg(encode(f.foto, 'base64')) AS fotos
+            SELECT p.id, p.nome_produto, p.descricao, p.categoria, p.nivel_ensino, p.valor, p.componente_curricular, 
+                   encode(p.pdf, 'base64') AS pdf,  -- Codificar o PDF para Base64
+                   COALESCE(json_agg(encode(f.foto, 'base64')) FILTER (WHERE f.foto IS NOT NULL), '[]') AS fotos  -- Codificar as fotos para Base64
             FROM produtos p
             LEFT JOIN fotos_produtos f ON p.id = f.produto_id
             GROUP BY p.id
@@ -31,5 +17,3 @@ export async function obterTodosProdutos() {
         throw error;
     }
 }
-
-
