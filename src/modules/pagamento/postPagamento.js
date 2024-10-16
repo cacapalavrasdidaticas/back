81b0c486dc333f416362d68035f5f11e61329885
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-10-16 00:39:32 -0300

    Update postPagamento.js

--- src/modules/pagamento/postPagamento.js
+++ src/modules/pagamento/postPagamento.js
@@ -77,7 +77,7 @@ async function enviarPagamento({ customer, billingType, value, dueDate, descript
       value,
       dueDate,
       description,
-      callback: {autoRedirect: true, successUrl: 'http://localhost:3000/Cart'}
+      callback: {autoRedirect: true, successUrl: 'http://localhost:3000/Cart/:algumValor'}
     }),
   };
 
