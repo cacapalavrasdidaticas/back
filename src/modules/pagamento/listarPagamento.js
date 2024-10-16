8f65e7385330b8e21b9a13dbb42119fa538e2a5f
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-10-15 21:34:00 -0300

    Add files via upload

---
+++ src/modules/pagamento/listarPagamento.js
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+import { getTokenById } from "../../tokenManager.js";
+
+dotenv.config();
+
+// Função para buscar pagamentos
+export async function getPagamentos() {
+  const url = `${process.env.ASAAS_SANDBOX_API_URL}/payments`; // Certifique-se que o URL está correto
+  const token = getTokenById(1); // Assumindo que o token é o mesmo do post
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      access_token: token
+    }
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const json = await response.json();
+
+    // Verifica se a resposta foi bem-sucedida
+    if (!response.ok) {
+      throw new Error(`Erro ao buscar pagamentos: ${json.message || 'Erro desconhecido'}`);
+    }
+
+    return json; // Retorna os dados de pagamentos
+  } catch (err) {
+    console.error('Erro ao buscar pagamentos:', err);
+    throw err;
+  }
+}
