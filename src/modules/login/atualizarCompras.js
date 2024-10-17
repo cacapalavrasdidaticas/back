7dfc8baa3c9a37f5aba4e0975a56990b6546ef2e
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-10-16 22:59:01 -0300

    Update atualizarCompras.js

--- src/modules/login/atualizarCompras.js
+++ src/modules/login/atualizarCompras.js
@@ -4,10 +4,10 @@ export async function atualizarComprasCliente(clientId, productIds) {
   try {
     const query = `
       UPDATE contas
-      SET compras = array_cat(coalesce(compras, '{}'), $1)
+      SET compras = array_cat(coalesce(compras, '{}'), $1::text[])
       WHERE id = $2
     `;
-    await db.query(query, [productIds, clientId]);
+    await db.query(query, [productIds.map(String), clientId]); // Convertendo os IDs para strings
     console.log('Compras atualizadas com sucesso para o cliente:', clientId);
   } catch (error) {
     console.error('Erro ao atualizar compras:', error);
