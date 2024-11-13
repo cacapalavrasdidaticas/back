ee402dc72498ad7200e2fb47f8872a039221cd76
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2024-11-12 22:02:11 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,8 @@ import pgp from "pg-promise";
 
 const db = pgp()({
   connectionString:
-    "postgres://umbugqae:ZA0JreAaEsvtGAyVglGqp9xg9bMYz95k@kesavan.db.elephantsql.com/umbugqae",
+    // "postgres://umbugqae:ZA0JreAaEsvtGAyVglGqp9xg9bMYz95k@kesavan.db.elephantsql.com/umbugqae",
+    "postgres://default:d8VTUYrwEXJ7@ep-bitter-frost-a4rijx1f.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
   ssl: {
     rejectUnauthorized: false,
   },
