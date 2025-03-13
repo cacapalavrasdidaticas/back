2949f139c11bd5342dea2d6ac03a9835758a3e15
cacapalavrasdidaticas <167832359+cacapalavrasdidaticas@users.noreply.github.com>
2025-03-12 23:14:55 -0300

    Update getDescription.js

--- src/modules/produtos/getDescription.js
+++ src/modules/produtos/getDescription.js
@@ -3,7 +3,7 @@ import db from "../../db.js";
 export async function obterTodosProdutosDescricao() {
     try {
         const produtos = await db.any(`
-            SELECT p.id, p.nome_produto, p.descricao, p.categoria, p.nivel_ensino, p.valor, p.componente_curricular,
+            SELECT p.id, p.nome_produto, p.descricao, p.categoria, p.nivel_ensino, p.valor, p.componente_curricular, p.url, p.selectedproducts,
                    json_agg(encode(f.foto, 'base64')) AS fotos
             FROM produtos p
             LEFT JOIN fotos_produtos f ON p.id = f.produto_id
